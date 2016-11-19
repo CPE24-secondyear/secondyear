ddd250d86478362165e7a9bff78d40f014cc2062
C475L4V3 <satethapan.naowasate@gmail.com>
2016-11-19 17:40:22 +0700

    started

---
+++ CPE24-secondyear/desktop/src/com/cpe24/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cpe24.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cpe24.game.main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main(), config);
+	}
+}
